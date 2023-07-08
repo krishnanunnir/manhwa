bbd16eea9916f27aa6ce244b9d6211df003b4764
krishnanunnir <krishnanunnir97@gmail.com>
2023-07-08 14:01:25 +0530

    basic setup of home page

--- web/views.py
+++ web/views.py
@@ -1,7 +1,14 @@
+from typing import Any, Dict
+
 from django.views.generic import TemplateView
 
-from .models import Blog, Manhwa, ManhwaList, Tags
+from .models import Manhwa
 
 
 class ManhwaTemplateView(TemplateView):
     template_name = "web/index.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+        ctx["manhwas"] = Manhwa.objects.all()[:10]
+        return ctx
